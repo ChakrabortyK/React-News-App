fa87d5c70b50838a09f0abe88e7be99e85112223
ChakrabortyK <chakrabortyk21offc@gmail.com>
2023-11-10 01:44:39 +0530

    V26 complete. Handling the newsitem and using the sample data)

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,47 +1,40 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import sample from './../sampleApi.json'
+
 
 export class News extends Component {
+    constructor() {
+        super();
+        this.state = {
+            article: sample.articles,
+            loading: false
+        }
+    }
+
+    // return <NewsItem key={element.title} title={element.title} description={element.description} imageUrl={element.urlToImage} />
+
     render() {
         return (
             <>
                 <div className="container my-3">
+
                     <h2>Top Headlines</h2>
-                    <div className="row my-3">
-                        <div className="col-md-4">
-                            <NewsItem title="first" description="Some desc" />
-                        </div>
-                        <div className="col-md-4">
-                            <NewsItem title="first" description="Some desc" />
-                        </div>
-                        <div className="col-md-4">
-                            <NewsItem title="first" description="Some desc" />
-                        </div>
-                    </div>
-                    <div className="row my-3">
-                        <div className="col-md-4">
-                            <NewsItem title="first" description="Some desc" />
-                        </div>
-                        <div className="col-md-4">
-                            <NewsItem title="first" description="Some desc" />
-                        </div>
-                        <div className="col-md-4">
-                            <NewsItem title="first" description="Some desc" />
-                        </div>
 
-                    </div>
-                    <div className="row my-3">
-                        <div className="col-md-4">
-                            <NewsItem title="first" description="Some desc" />
-                        </div>
-                        <div className="col-md-4">
-                            <NewsItem title="first" description="Some desc" />
-                        </div>
-                        <div className="col-md-4">
-                            <NewsItem title="first" description="Some desc" />
-                        </div>
 
+                    <div className="row my-3">
+                        {this.state.article.map(element => {
+                            return <div className="col-lg-4" key={element.url}>
+                                <NewsItem
+
+                                    title={element.title.slice(0, 52)}
+                                    description={element.description.slice(0, 100)}
+                                    imageUrl={element.urlToImage}
+                                    url={element.url} />
+                            </div>
+                        })}
                     </div>
+
                 </div>
             </>
         )
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -3,15 +3,17 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
     render() {
 
-        let { title, description } = this.props;
+        let { title, description, imageUrl, url } = this.props;
         return (
             <>
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src="https://onecricketnews.akamaized.net/parth-editor/oc-dashboard/news-images-prod/1699357071216_Untitled design - 2023-11-07T170619.923.jpg" className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="/" className="btn btn-primary">Go somewhere</a>
+                <div className='my-3'>
+                    <div className="card" style={{ width: "18vw" }}>
+                        <img src={imageUrl} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{title}</h5>
+                            <p className="card-text">{description}</p>
+                            <a href={url} className="btn btn-sm btn-primary">Read More</a>
+                        </div>
                     </div>
                 </div>
             </>
