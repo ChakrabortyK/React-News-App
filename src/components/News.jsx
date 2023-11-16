98d8fdc2f1ea42a248e19adef39e1757ebcd6446
ChakrabortyK <chakrabortyk21offc@gmail.com>
2023-11-16 22:01:04 +0530

    V28 complete fetching and pagination

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,19 +1,50 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
-import sample from './../sampleApi.json'
+// import sample from './../sampleApi.json'
 
 
 export class News extends Component {
     constructor() {
         super();
         this.state = {
-            article: sample.articles,
+            // article: sample.articles,
+            page: 1,
+            articles: [],
             loading: false
         }
     }
 
-    // return <NewsItem key={element.title} title={element.title} description={element.description} imageUrl={element.urlToImage} />
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=d150430bb4bf40c396ca36687f2676e2&pageSize=9";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+    }
 
+
+    handleNextClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=d150430bb4bf40c396ca36687f2676e2&page=${this.state.page + 1}&pageSize=9`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            page: this.state.page + 1,
+            articles: parsedData.articles
+        })
+        // console.log(this.state)
+    }
+    handlePrevClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=d150430bb4bf40c396ca36687f2676e2&page=${this.state.page - 1}&pageSize=9`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles,
+
+        })
+        // console.log(this.state)
+        // console.log("prev")
+
+    }
     render() {
         return (
             <>
@@ -23,17 +54,24 @@ export class News extends Component {
 
 
                     <div className="row my-3">
-                        {this.state.article.map(element => {
+                        {this.state.articles.map(element => {
+                            // console.log(element)
                             return <div className="col-lg-4" key={element.url}>
                                 <NewsItem
 
-                                    title={element.title.slice(0, 52)}
-                                    description={element.description.slice(0, 100)}
+                                    title={element.title ? element.title.slice(0, 45) : ""}
+                                    description={element.description ? element.description.slice(0, 88) : ""}
                                     imageUrl={element.urlToImage}
                                     url={element.url} />
                             </div>
                         })}
                     </div>
+
+                    <div className="container d-flex justify-content-between">
+                        <button type="button" disabled={this.state.page <= 1} className="btn btn-outline-info btn-sm" onClick={this.handlePrevClick}>&larr; Previous Page</button>
+                        <button type="button" disabled={this.state.page > Math.ceil(this.state.totalResults / 9)} className="btn btn-outline-info btn-sm" onClick={this.handleNextClick}>&rarr; Next Page</button>
+                    </div>
+
 
                 </div>
             </>
