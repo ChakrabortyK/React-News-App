33328caf604780c6f501084c84b981c2711889de
ChakrabortyK <chakrabortyk21offc@gmail.com>
2023-11-18 19:59:08 +0530

    v30 complete. Adding catetgories in navbar. refer to this Version before any router setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 // import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+// import { Link } from "react-router-dom";
+
 
 export class Navbar extends Component {
     // static propTypes = {}
@@ -14,18 +16,19 @@ export class Navbar extends Component {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
-                            <li className="nav-item">
+                            <li className="nav-item mx-2">
                                 <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">About Page</a>
+                            <li className="nav-item mx-2"><a className="nav-link" href="/">About Page</a>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Pricing</a>
-                            </li>
-                            {/* <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
-                            </li> */}
+                            <li className="nav-item mx-2"><a className="nav-link" href="/">business</a></li>
+                            <li className="nav-item mx-2"><a className="nav-link" href="/">entertainment</a></li>
+                            <li className="nav-item mx-2"><a className="nav-link" href="/">general</a></li>
+                            <li className="nav-item mx-2"><a className="nav-link" href="/">health</a></li>
+                            <li className="nav-item mx-2"><a className="nav-link" href="/">science</a></li>
+                            <li className="nav-item mx-2"><a className="nav-link" href="/">sports</a></li>
+                            <li className="nav-item mx-2"><a className="nav-link" href="/">technology</a></li>
+
                         </ul>
                     </div>
                 </div>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,9 +2,21 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import { BarLoader } from 'react-spinners'
 // import sample from './../sampleApi.json'
-
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+
+    static defaultProps = {
+        country: 'in',
+        pageSize: 9,
+        category: 'general'
+    }
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string
+    }
+
     constructor() {
         super();
         this.state = {
@@ -16,7 +28,7 @@ export class News extends Component {
     }
 
     async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=d150430bb4bf40c396ca36687f2676e2&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d150430bb4bf40c396ca36687f2676e2&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -25,7 +37,7 @@ export class News extends Component {
 
 
     handleNextClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=d150430bb4bf40c396ca36687f2676e2&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d150430bb4bf40c396ca36687f2676e2&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -37,7 +49,7 @@ export class News extends Component {
         // console.log(this.state)
     }
     handlePrevClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=d150430bb4bf40c396ca36687f2676e2&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d150430bb4bf40c396ca36687f2676e2&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -61,7 +73,7 @@ export class News extends Component {
                         <button type="button" disabled={this.state.page > Math.ceil(this.state.totalResults / 9)} className="btn btn-outline-info btn-sm" onClick={this.handleNextClick}>&rarr; Next Page</button>
                     </div>
 
-                    <h2 className='text-center'>Top Headlines</h2>
+                    <h2 className='text-center my-3'>Top Headlines</h2>
                     {this.state.loading && <BarLoader
                         color="#36d7b7"
                         cssOverride={{
